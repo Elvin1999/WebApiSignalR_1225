884b81d0b7c8fe4a15d730b311c51634ac7f81ab
camalzadeelvin <camalzadeelvin@STHQ0118-01>
2023-12-08 11:52:41 +0400

    Add project files.

---
+++ WebApiSignalR/Controllers/OfferController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiSignalR.Helpers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiSignalR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OfferController : ControllerBase
+    {
+        public OfferController()
+        {
+            
+        }
+        // GET: api/<OfferController>
+        [HttpGet]
+        public double Get()
+        {
+            return FileHelper.Read();
+        }
+
+        // GET: api/<OfferController>
+        [HttpGet("/Increase")]
+        public void Increase(double data)
+        {
+            var result=FileHelper.Read()+data;
+            FileHelper.Write(result);   
+        }
+
+        // GET api/<OfferController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<OfferController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<OfferController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<OfferController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApiSignalR/Hubs/MessageHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using WebApiSignalR.Helpers;
+
+namespace WebApiSignalR.Hubs
+{
+    public class MessageHub:Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Others.SendAsync("ReceiveConnectInfo", "User Connected");
+        }
+
+        public async Task SendMessage(string message)
+        {
+            await Clients.Others.SendAsync("ReceiveMessage",message+"'s Offer : ",FileHelper.Read());
+        }
+
+        public async Task SendWinnerMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveInfo", message, FileHelper.Read());
+        }
+    }
+}
